87c6e294b8d72831021560e2dec776d3f546b672
Serhii Skrypnik <eyerediskin@gmail.com>
2022-07-30 21:57:23 +0300

    Add resource types and controllers
    Kinds created: DHCPServer DHCPSubnet DHCPHost DHCPLeases
    
    operator-sdk create api \
      --group dhcp \
      --version v1alpha1 \
      --kind $KIND \
      --resource \
      --controller

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	dhcpv1alpha1 "github.com/bmcgo/k8s-dhcp/api/v1alpha1"
+	"github.com/bmcgo/k8s-dhcp/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(dhcpv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,34 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.DHCPServerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DHCPServer")
+		os.Exit(1)
+	}
+	if err = (&controllers.DHCPSubnetReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DHCPSubnet")
+		os.Exit(1)
+	}
+	if err = (&controllers.DHCPHostReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DHCPHost")
+		os.Exit(1)
+	}
+	if err = (&controllers.DHCPLeasesReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DHCPLeases")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
